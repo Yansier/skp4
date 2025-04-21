a521062e9a88867668d5b9daf67addf2894e0e08
Yansier <ledermanncaravetta968@gmail.com>
2025-04-21 11:53:34 +0800

    0421goA

--- src/admin/views/product/index.vue
+++ src/admin/views/product/index.vue
@@ -23,21 +23,20 @@
           </el-row>
         </el-form>
         <el-button class="mb-4" @click="dialogVisible = true" type="primary">新增</el-button>
-        <app-table
-          ref="tableRef"
-          url="/sys/product/item/list"
-          :query-params="queryParams"
-        >
+        <app-table ref="tableRef" url="/sys/product/item/list" :query-params="queryParams">
           <!--      <el-table-column label="商品ID" prop="goodsId" />-->
           <el-table-column label="标题" prop="title" min-width="300" />
           <el-table-column label="封面" prop="imgSrc">
-            <template #default="{row}">
+            <template #default="{ row }">
               <img class="cursor-pointer" :src="row.imgSrc" alt="" width="40" @click="handlePreview(row.imgSrc)">
             </template>
           </el-table-column>
+          <el-table-column label="商品名称" prop="goodsName" />
           <el-table-column label="商品分类" prop="categoryName" />
+          <el-table-column label="商品详情" prop="goodsDetail" />
           <el-table-column label="进货价" prop="purchase" />
           <el-table-column label="标价" prop="listPrice" />
+          <el-table-column label="定价" prop="fixPrice" />
           <el-table-column label="售价" prop="sellingPrice" />
           <el-table-column label="操作" width="120">
             <template #default="{ row }">
@@ -50,51 +49,76 @@
       <el-tab-pane label="商品分类" name="category">
         <category-com />
       </el-tab-pane>
+      <el-tab-pane label="首页控制" name="homedata">
+        <ControlCom />
+      </el-tab-pane>
+      <el-tab-pane label="联系方式更改" name="linkpage">
+        <linkpage />
+      </el-tab-pane>
     </el-tabs>
   </section>
   <el-dialog v-model="dialogVisible" :close-on-click-modal="false" :close-on-press-escape="false" title="编辑">
-    <el-form ref="formRef" :model="editData" label-width="auto">
-      <el-form-item label="标题" prop="title">
-        <el-input v-model="editData.title" type="text" placeholder="请输入"></el-input>
-      </el-form-item>
-      <el-form-item label="详情图" prop="imgsSrc">
-        <div class="flex flex-wrap">
-          <img class="rounded-s w-1/3 border-box p-1 cursor-pointer" v-for="item in editData.imgsSrc" :src="item" :key="item" alt="" @click="handlePreview(item)">
-        </div>
-      </el-form-item>
-      <el-form-item label="搜索码" prop="markCode">
-        <el-input v-model="editData.markCode" type="text" placeholder="请输入"></el-input>
-      </el-form-item>
-      <el-form-item label="商品分类" prop="categoryId">
-        <el-select v-model="editData.categoryId" filterable>
-          <el-option v-for="item in categoryOptions" :key="item.id" :label="item.name" :value="item.id" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="进货价" prop="purchase">
-        <el-input-number v-model="editData.purchase" :precision="2"></el-input-number>
-      </el-form-item>
-      <el-form-item label="标价" prop="listPrice">
-        <el-input-number v-model="editData.listPrice" :precision="2"></el-input-number>
-      </el-form-item>
-      <el-form-item label="售价" prop="sellingPrice">
-        <el-input-number v-model="editData.sellingPrice" :precision="2"></el-input-number>
-      </el-form-item>
-      <el-form-item>
-        <div class="w-full text-right">
-          <el-button @click="handleCancel">取消</el-button>
-          <el-button @click="handleSubmit" type="primary">确定</el-button>
-        </div>
-      </el-form-item>
-    </el-form>
+    <el-skeleton :loading="loading" animated>
+      <template #default>
+        <el-form ref="formRef" :model="editData" label-width="auto">
+          <el-form-item label="标题" prop="goodsName">
+            <el-input v-model="editData.goodsName" type="text" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="名称" prop="title">
+            <el-input v-model="editData.title" type="text" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="详情" prop="title">
+            <el-input v-model="editData.goodsDetail" type="text" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="详情图" prop="imgsSrc">
+            <el-upload action="" list-type="picture-card" :file-list="fileList" :auto-upload="false"
+              :on-preview="handlePreview" :on-remove="handleRemove" :on-change="uploadChange"
+              :before-upload="beforeUpload" accept="image/*">
+              <i class="el-icon-plus" />
+              +
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="搜索码" prop="markCode">
+            <el-input v-model="editData.markCode" type="text" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="商品分类" prop="categoryId">
+            <el-select v-model="editData.categoryId" filterable>
+              <el-option v-for="item in categoryOptions" :key="item.id" :label="item.name" :value="item.id" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="进货价" prop="purchase">
+            <el-input-number v-model="editData.purchase" :precision="2"></el-input-number>
+          </el-form-item>
+          <el-form-item label="定价" prop="fixPrice">
+            <el-input-number v-model="editData.fixPrice" :precision="2"></el-input-number>
+          </el-form-item>
+          <el-form-item label="标价" prop="listPrice">
+            <el-input-number v-model="editData.listPrice" :precision="2"></el-input-number>
+          </el-form-item>
+          <el-form-item label="售价" prop="sellingPrice">
+            <el-input-number v-model="editData.sellingPrice" :precision="2"></el-input-number>
+          </el-form-item>
+          <el-form-item>
+            <div class="w-full text-right">
+              <el-button @click="handleCancel">取消</el-button>
+              <el-button @click="handleSubmit" type="primary">确定</el-button>
+            </div>
+          </el-form-item>
+        </el-form>
+      </template>
+    </el-skeleton>
+
   </el-dialog>
 </template>
 
 <script setup>
 import AppTable from '@/admin/components/table/index.vue'
 import CategoryCom from '@/admin/views/product/components/category.vue'
+import ControlCom from '@/admin/views/product/components/control.vue'
+import linkpage from '@/admin/views/product/components/linkpage.vue'
 import { getCurrentInstance, onMounted, ref } from 'vue'
 import { operateProduct, categoryList, deleteProduct, productDetail } from '@/admin/api/index.js'
-import { ElMessage, ElMessageBox } from 'element-plus'
+import { ElMessage, ElMessageBox, ElSkeleton } from 'element-plus'
 
 const activeTab = ref('product')
 const searchForm = ref({
@@ -116,6 +140,7 @@ const formRef = ref(null)
 const categoryOptions = ref([])
 const fileList = ref([])
 const app = getCurrentInstance()
+const loading = ref(false)
 
 const queryParams = () => {
   const { keyword, categoryId } = searchForm.value
@@ -126,7 +151,6 @@ const queryParams = () => {
 }
 
 const onRemove = (file) => {
-  console.log(fileList.value,file)
   fileList.value = fileList.value.filter(item => item.uid !== file.uid)
 }
 const uploadChange = (file, list) => {
@@ -142,8 +166,22 @@ const handleReset = () => {
   }
   handleSearch()
 }
+
+const beforeUpload = (file) => {
+  const isImage = file.type.startsWith('image/')
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isImage) ElMessage.error('只能上传图片格式！')
+  if (!isLt2M) ElMessage.error('图片大小不能超过 2MB！')
+  return isImage && isLt2M
+}
+const handleRemove = (file, fileListNow) => {
+  fileList.value = fileListNow
+}
 const handleEdit = (row) => {
+  loading.value = true
+  
   productDetail(row.goodsId).then(res => {
+    
     editData.value = res.data
     fileList.value = res.data.imgsSrc.map(item => {
       return {
@@ -151,8 +189,10 @@ const handleEdit = (row) => {
         url: item
       }
     })
+    dialogVisible.value = true
+  }).finally(() => {
+    loading.value = false
   })
-  dialogVisible.value = true
 }
 const handleDelete = (row) => {
   ElMessageBox.confirm('确定删除吗？', '提示', {
@@ -162,7 +202,7 @@ const handleDelete = (row) => {
       ElMessage.success('删除成功')
       tableRef.value.reload(false)
     })
-  }).catch(() => {})
+  }).catch(() => { })
 }
 
 const handleCancel = () => {
